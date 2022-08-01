031a28a12acccb22bc4616b20f712a26a700729e
TMBrunetti <tmb2192@gmail.com>
2022-08-01 13:28:48 -0400

    created repo and backend framework

---
+++ BackEnd/codetari/src/main/java/com/codetari/CodetariApplication.java
@@ -0,0 +1,13 @@
+package com.codetari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetariApplication.class, args);
+	}
+
+}
