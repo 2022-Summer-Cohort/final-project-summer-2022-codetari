14cbae622f8e6cfbd9967fd8f7118623f9451f0a
chandanbihagpatel <chandanbihagpatel@gmail.com>
2022-08-04 10:50:12 -0400

    adsf

--- BackEnd/src/main/java/com/codetari/CodetariApplication.java
+++ BackEnd/src/main/java/com/codetari/CodetariApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class codetariApplication {
+public class CodetariApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(codetariApplication.class, args);
+		SpringApplication.run(CodetariApplication.class, args);
 	}
 
 }
