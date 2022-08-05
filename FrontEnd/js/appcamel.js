3481fab24170febf5ed5d0933249ca00ff13978c
FerieDagnew <feriefikir@gmail.com>
2022-08-05 16:38:03 -0400

    update

--- FrontEnd/js/appcamel.js
+++ FrontEnd/js/appcamel.js
@@ -3,6 +3,20 @@ const camel = document.querySelector(".camel")
 let keys = {ArrowsUp: false, ArrowDown: false, Arrowleft: false, ArrowRight: false};
 let x = 50;
 let y = 90;
+const redcar = document.querySelector(".red")
+const greencar = document.querySelector(".green")
+const bluecar = document.querySelector(".blue")
+const yellowcar = document.querySelector(".yellow")
+// const redcarpath = redcar.querySelector(".carcolor")
+// const greencarpath = greencar.querySelector(".carcolor")
+// const bluecarpath = bluecar.querySelector(".carcolor")
+// const yellowcarpath = yellowcar.querySelector(".carcolor")
+
+// redcar.getSVGDocument().getElementById("carcolor").setAttribute("fill","red")
+// bluecar.setAttribute("fill","blue")
+greencar.contentDocument().getElementById("carcolor").setAttribute("fill","green")
+// yellowcar.getSVGDocument().getElementById("carcolor").setAttribute("fill","yellow")
+
 
 
 document.addEventListener('keyup', (e) => {
@@ -11,6 +25,12 @@ document.addEventListener('keyup', (e) => {
         console.log("arrow key pressed") 
         x-=20  
         camel.style.transform =`translate(${x}px,${y}vh)`
+
+  
+            case 'ArrowRight' :
+            console.log("arrow key pressed") 
+            x+=20  
+            camel.style.transform =`translate(${x}px,${y}vh)`
             
 
     }
