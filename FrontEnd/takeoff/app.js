a141e083a5d6e866a71645c309fb554dd3b4acf7
Patrick Hastings <phastings82@gmail.com>
2022-08-09 14:01:53 -0400

    updated liftoff game

--- FrontEnd/takeoff/app.js
+++ FrontEnd/takeoff/app.js
@@ -1,15 +1,124 @@
-const tl = gsap.timeline({defaults: {duration: 1}})
-const t2 = gsap.timeline({defaults: {duration: .5}})
+import question from "./questions.js"
+
+const container = document.querySelector(".container")
+const playBtn = document.querySelector(".playGame")
+const scoreEl = document.querySelector(".score");
 
 
-const up = document.querySelector(".up");
-const down = document.querySelector(".down");
-up.addEventListener('click', ()=>{
-    tl.to('.bg', {y: '+=500px'})
+class Player {
+    constructor() {
+        this._score = 0;
+    }
+    get score() {
+        return this._score;
+    }
+    addScore() {
+        this._score ++;
+    }
+}
+
+const player = new Player();
+
+function initiateCollision(){
+    const element = document.querySelector('spaceship');
+    const position = element.getBoundingClientRect();
+    const x = position.left;
+    const y = position.top;
+    console.log(x, y)
+    if (player._score > 1){
+        if ('.bg', {y: '0'}) {
+            console.log("GAMEOVER");
+        } 
+    }
+}
+
+
+playBtn.addEventListener("click", ()=>{
+    getRandomId();
+})
+function getRandomId(){
+    fetch ("http://localhost:8080/api/qA")
+    .then(res => res.json())
+    .then(qas => {
+        let ids = [];
+        qas.forEach(qa =>{
+            const idField = qa.id;
+            ids.push(idField);
+        })
+        let randomId = ids[Math.floor(Math.random()*ids.length)];
+        createQuestion(randomId)
+        console.log(randomId)
+    })
+}
+function makeQuestionView(q){
+    body.innerHtml = question(q);
+}
+
+function createQuestion(randomId){
+    fetch(`http://localhost:8080/api/qA/${randomId}`)
+        .then(res => res.json())
+        .then(q =>{
+            displayQuestion(q)
+            console.log(q)
+        })
+}
+
+function displayQuestion(q){
+    
+    container.innerHTML += question(q);
+    console.log(q)
+    const submitBtn = document.querySelector(".submit");
+    const answers = document.querySelector("#correctAnswer");
+    submitBtn.addEventListener('click', ()=>{
+        if(answers.checked){
+            player.addScore();
+            getRandomId();
+            animeUp();
+            down();
+            initiateCollision();
+            container.innerHTML = ""
+            console.log(player._score);
+            console.log(scoreEl);
+            scoreEl.innerHTML = player._score;
+            
+
+        }
+        else{
+            container.innerHTML = ""
+            getRandomId();
+        }
+    })
+}
+
+
+
+
+
+
+
+
+function animeUp(){
+    
+    const tl = gsap.timeline({defaults: {duration: 5}})
+    const t2 = gsap.timeline({defaults: {duration: .5}})
+    tl.to('.bg', {y: '+=500px', ease: "power4.out"})
     t2.to('#flames', {opacity: 1})
     t2.to('#flames', {opacity: 0})
+}
 
-})
-down.addEventListener('click', ()=>{
+function animeDown(){
+    const tl = gsap.timeline({defaults: {duration: 5}})
     tl.to('.bg', {y: '0'})
-})
+}
+
+function down(){
+    const tl = gsap.timeline({defaults: {duration: 5}})
+
+    tl.to('.bg', {y: '0', delay: 5, ease: "power4.in"})
+    
+}
+
+
+
+
+
---
+++ FrontEnd/takeoff/questions.js
@@ -0,0 +1,27 @@
+export default function question(q){
+    console.log(q.question)
+    console.log(q.wrongA)
+    return `
+    <div id="questionPop" class="overlay">
+    <div class="popup">
+        <div class="content">
+            <div class="questionBox">
+                <h2 class="question">${q.question}</h2>
+                    <div class="form">
+                    <label for="right">${q.answer}</label>
+                    <input type="radio" name="multi" value="correct" id="correctAnswer">
+                    ${q.wrongA.map(wrong => {
+                        return `
+                    <label for="wrong3">${wrong}</label>
+                    <input type="radio" name="multi" value="wrongAnswer" id="incorrect"></input>
+                    `
+                    }).join("")
+                }
+                    <button type="submit" class="submit">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+}
