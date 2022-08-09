4c08a892afc6b713cb6157c94f848c780665d937
chandanbihagpatel <chandanbihagpatel@gmail.com>
2022-08-09 11:25:47 -0400

    still needs gameover

--- FrontEnd/space-invaders/app.js
+++ FrontEnd/space-invaders/app.js
@@ -4,14 +4,17 @@ import question from "./questions.js"
 const container = document.querySelector(".container")
 const playBtn = document.querySelector(".playGame")
 let qCount = 0;
-
+let wCount = 1;
+if(qCount > 2){
+    playGame(qCount, wCount)
+}
+console.log(qCount)
 
 playBtn.addEventListener("click", ()=>{
     getRandomId();
-    playGame();
+    playGame(qCount);
 })
 function getRandomId(){
-    qCount++;
     fetch ("http://localhost:8080/api/qA")
     .then(res => res.json())
     .then(qas => {
@@ -37,16 +40,44 @@ function displayQuestion(q){
     container.innerHTML += question(q);
     console.log(q)
     const submitBtn = document.querySelector(".submit");
-    const answers = document.querySelector("#correctAnswer");
+    const correctAnswer = document.querySelector(".rightAnswer");
+    const answerInput = document.querySelectorAll(".answerInput");
     submitBtn.addEventListener('click', ()=>{
-        if(answers.checked){
+        let t = 0;
+        answerInput.forEach(input =>{
+            if(input.checked && input.value == correctAnswer.value){
+                t++;
+                const tl = gsap.timeline({defaults: {duration: 1}})
+            const t2 = gsap.timeline({defaults: {duration: 1}})
+
+            tl.to('.correctAnswer', {opacity: 1})
+            t2.to('.correctAnswer', {opacity: 0, delay: 1})
+
+            qCount ++;
             container.innerHTML = ""
             getRandomId();
-            qcount ++;
-        }
-        else{
+            if(qCount == 3){
+                const tl = gsap.timeline({defaults: {duration: 1}})
+                const t2 = gsap.timeline({defaults: {duration: 1}})
+
+                tl.to('.upgrade', {opacity: 1})
+                t2.to('.upgrade', {opacity: 0, delay: 1})
+                playGame(qCount, wCount)
+            }
+            }
+        })
+        if(t==0){
+            wCount ++;
+            const tl = gsap.timeline({defaults: {duration: 1}})
+            const t2 = gsap.timeline({defaults: {duration: 1}})
+
             container.innerHTML = ""
+            tl.to('.wrongAnswer', {opacity: 1})
+            t2.to('.wrongAnswer', {opacity: 0, delay: 1})
             getRandomId();
+            if(wCount == 2){
+                playGame(qCount, wCount)
+            }
         }
 
     })
