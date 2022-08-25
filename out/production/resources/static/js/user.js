38d332245f3fbc6ff78b6099d0e1177678cb8173
TaylourH <harristaylour@gmail.com>
2022-08-25 13:56:39 -0400

    11

--- out/production/resources/static/js/user.js
+++
@@ -1,9 +0,0 @@
-export default class user{
-    constructor(level, language){
-        this.level = level
-        this.language = language
-    }
-    getLevel(){
-        return this.level;
-    }
-}
--- out/production/resources/static/takeoff/score.js
+++
@@ -1,5 +0,0 @@
-export default function scoreview(){
-    return `
-    <div class="split left"><span class="">..//PREPARE FOR LIFTOFF!..</span></div>
-    `
-}
